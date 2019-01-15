b88fbc76b037fe233a9c1ab2caa8d8364c5cf99b
TJ Jackson <elitefourfresh@gmail.com>
2019-01-15 11:15:05 -0500

    basic functionality done

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 // Click On X To Delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text'").on("keypress", function(event){
+    if(event.which === 13){
+        // Grabbing New Todo Text From Input
+        var todoText = $(this).val();
+        $(this).val("");
+        // Create New li And Add To ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    };
 });
